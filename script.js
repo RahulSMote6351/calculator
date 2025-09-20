512acbb37b4be97e7581342597af9e0c8888c7d4
RahulSMote6351 <rmote6351@gmail.com>
2025-09-20 16:41:08 +0530

    add files

---
+++ script.js
@@ -0,0 +1,17 @@
+
+      let input = document.querySelector("#inputprompt");
+      let result = document.querySelector(".result");
+      let buttons = document.querySelectorAll("button");
+
+      function calculaterResult(ele) {
+        let exp = `${input.value}${ele.innerText}`;
+        input.value =exp;
+
+        if(["+","-","*","/","%","."].includes(ele.innerText)) return;
+        result.value = eval(exp);
+      }
+
+      function clearAll(){
+        input.value= "";
+        result.value= "";
+      }
